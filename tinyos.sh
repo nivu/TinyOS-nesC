982ade6e1111e1c7943dd18707810600cd53e195
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-06-19 02:54:37 -0700

    repo change

--- tinyos.sh
+++ tinyos.sh
@@ -1,6 +1,6 @@
 sudo apt install openjdk-8-jdk gcc-msp430 nescc make automake autoconf build-essential python-pip git
 
-git clone https://github.com/tinyos/tinyos-main.git
+git clone https://github.com/navneetnivu07/tinyos-main.git
 
 cd tinyos-main
 cd tools
